217cd7951a61fdfe9ec472fed4dfcea2d739cd91
Steven Deibert <stevekdeibert@gmail.com>
2026-09-08 20:33:37 -0700

    Rename internal mod ID to ae2_tag_preferences

--- src/main/java/com/startechnology/compat/universalcircuitpatterns/UniversalCircuitPatterns.java
+++ src/main/java/com/startechnology/compat/universalcircuitpatterns/UniversalCircuitPatterns.java
@@ -7,7 +7,7 @@ import net.minecraftforge.fml.config.ModConfig;
 
 @Mod(UniversalCircuitPatterns.MOD_ID)
 public final class UniversalCircuitPatterns {
-    public static final String MOD_ID = "start_universal_circuit_patterns";
+    public static final String MOD_ID = "ae2_tag_preferences";
 
     public UniversalCircuitPatterns() {
         ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, PresetConfig.SPEC, "ae2-tag-preferences-client.toml");
